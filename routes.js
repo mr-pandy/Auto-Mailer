c49ac9425c04a33052b4544037840a7ce27812ee
mr-pandy <ekerekepreciousimeh@gmail.com>
2020-08-03 03:03:40 +0100

    Construction

--- routes.js
+++ routes.js
@@ -5,4 +5,8 @@ module.exports = (app)=>{
      app.get('/', (req,res)=>{
           res.render("home")
       });
+
+      app.get('/sign-up', (req,res)=>{
+          res.render("sign-up")
+      });
 };
