761da568ccc13aed36b8baf3e585e59338c2569a
mr-pandy <ekerekepreciousimeh@gmail.com>
2020-08-03 01:04:42 +0100

    Testing route for Auto Mailer

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const ejs = require('ejs');
+const nodeMailer = require('nodemailer');
+let port = process.env.PORT || 3000;
+
+//Route
+const routes = require(`${__dirname}/routes.js`);
+
+// Initialize
+const app = express();
+app.set("view engine", "ejs");
+     app.use(express.static('public'));
+routes(app)
+
+app.listen(port, () => {
+    console.log('Port running on 3000');
+})
